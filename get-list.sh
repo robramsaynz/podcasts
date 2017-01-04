c4c8a5a5778e09288871d7b8df139f8b7b96de62
Rob Ramsay <robert.ramsay.nz@gmail.com>
2017-01-04 18:28:29 +1300

    split podcast script into seperate scripts

---
+++ get-list.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# ./get-list.sh >> list.txt
+#
+
+#curl http://rinse.fm/podcasts/ | sed -E -n -e '/download="http:\/\/podcast[^ ]*/p' | sed -E -e 's/.*download="//g' -e 's/" .*//' > list.txt
+#curl http://rinse.fm/podcasts/?page=2 | sed -E -n -e '/download="http:\/\/podcast[^ ]*/p' | sed -E -e 's/.*download="//g' -e 's/" .*//' >> list.txt
+#curl http://rinse.fm/podcasts/?page=3 | sed -E -n -e '/download="http:\/\/podcast[^ ]*/p' | sed -E -e 's/.*download="//g' -e 's/" .*//' >> list.txt
+
--- list-to-names.sh
+++ list-to-names.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+#
+# ./list-to-names.sh list.txt
+#
 
-#curl http://rinse.fm/podcasts/ | sed -E -n -e '/download="http:\/\/podcast[^ ]*/p' | sed -E -e 's/.*download="//g' -e 's/" .*//' > list.txt
-#curl http://rinse.fm/podcasts/?page=2 | sed -E -n -e '/download="http:\/\/podcast[^ ]*/p' | sed -E -e 's/.*download="//g' -e 's/" .*//' >> list.txt
-#curl http://rinse.fm/podcasts/?page=3 | sed -E -n -e '/download="http:\/\/podcast[^ ]*/p' | sed -E -e 's/.*download="//g' -e 's/" .*//' >> list.txt
+file="$1"
 
 gawk '{
     match($0, /([^\/]*)(......)\.mp3/, rslt);
@@ -12,24 +13,5 @@ gawk '{
     "date -u -jf %d%m%y " date " +%Y-%m-%d" | getline shortdate
 
     print shortdate"  "performer;
-}' list.txt
-
-gawk '$1 ~ /Uncle/ {
-    url = $0;
-    guid = $0;
-
-    match($0, /([^\/]*)(......)\.mp3/, rslt);
-    performer = rslt[1];
-    date = rslt[2];
-
-    "date -u -jf %d%m%y%H%M " date "0000" | getline longdate
-    "date -u -jf %d%m%y " date " +%Y-%m-%d" | getline shortdate
-
-    print "    <item>";
-    print "      <title>"shortdate"  "performer"</title>";
-    print "      <enclosure url=\""url"\" type=\"audio/mpeg\" length=\"1\"/>";
-    print "      <guid isPermaLink=\"false\">"guid"</guid>";
-    print "      <pubDate>"longdate"</pubDate>";
-    print "    </item>";
-}' list.txt
+}' "$file"
 
