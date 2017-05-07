5c71c32d884bc58bb742f32c1db9f62d5e00c72d
Rob Ramsay <robert.ramsay.nz@gmail.com>
2017-05-07 22:53:02 +1200

    remove shell scripts

--- get-list.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# ./get-list.sh >> list.txt
-#
-
-curl http://rinse.fm/podcasts/ | sed -E -n -e '/download="http:\/\/podcast[^ ]*/p' | sed -E -e 's/.*download="//g' -e 's/" .*//' > list.txt
-curl http://rinse.fm/podcasts/?page=2 | sed -E -n -e '/download="http:\/\/podcast[^ ]*/p' | sed -E -e 's/.*download="//g' -e 's/" .*//' >> list.txt
-curl http://rinse.fm/podcasts/?page=3 | sed -E -n -e '/download="http:\/\/podcast[^ ]*/p' | sed -E -e 's/.*download="//g' -e 's/" .*//' >> list.txt
-
