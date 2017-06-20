d1267e9090de62aaf6c21cf484b6bac6c1e2bd9e
Rob Ramsay <robert.ramsay.nz@gmail.com>
2017-06-21 01:43:41 +1200

    fix check-rss.sh

--- check-rss.sh
+++ check-rss.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #
-# Usage:  ./check-rss.sh manual.rss
+# Usage:  ./check-rss.sh
 
 check() {
   echo "--- $1 ---"
   echo
 
-  http http://www.feedvalidator.org/check.cgi\?url\=https%3A%2F%2Frobramsaynz.github.io%2Fpodcasts%2F$file \
+  http http://www.feedvalidator.org/check.cgi\?url\=https%3A%2F%2Frobramsaynz.github.io%2Fpodcasts%2F$1 \
   | gawk '/This is a valid RSS feed/ {print} /This feed does not validate/ {print} /interoperability with the widest range/ {print} /implementing the following recommendations/ {print}' \
   | html2text
 
